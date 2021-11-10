3d3250802b7d52b4e6edea11cf99183fa60af53b
Pawel Mroz <pawel.mroz@ingeniousio.com>
2021-11-10 17:23:45 +0100

    Added dropdown menu

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -19,58 +19,79 @@
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
 <body class="bg-gray-100">
-    <div id="app">
-        <nav class="bg-white">
-            <div class="container mx-auto">
-                <div class="flex justify-between items-center py-2">
-                    <a class="navbar-brand" href="{{ url('/') }}">
-                        {{ config('app.name') }}
-                    </a>
+<div id="app">
+    <nav class="bg-white">
+        <div class="container mx-auto">
+            <div class="flex justify-between items-center py-2">
+                <a class="navbar-brand" href="{{ url('/') }}">
+                    {{ config('app.name') }}
+                </a>
 
-                    <div>
-                        <!-- Right Side Of Navbar -->
-                        <ul class="navbar-nav ml-auto">
-                            <!-- Authentication Links -->
-                            @guest
-                                @if (Route::has('login'))
-                                    <li class="nav-item">
-                                        <a class="nav-link" href="{{ route('login') }}">Zaloguj się</a>
-                                    </li>
-                                @endif
-
-                                @if (Route::has('register'))
-                                    <li class="nav-item">
-                                        <a class="nav-link" href="{{ route('register') }}">Zarejestruj się</a>
-                                    </li>
-                                @endif
-                            @else
-                                <li class="nav-item dropdown">
-                                    <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                        {{ Auth::user()->name }}
-                                    </a>
-
-                                    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                        <a class="dropdown-item" href="{{ route('logout') }}"
-                                           onclick="event.preventDefault();
-                                                         document.getElementById('logout-form').submit();">
-                                            {{ __('Logout') }}
-                                        </a>
-
-                                        <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                            @csrf
-                                        </form>
-                                    </div>
+                <div>
+                    <!-- Right Side Of Navbar -->
+                    <ul class="flex">
+                        <!-- Authentication Links -->
+                        @guest
+                            @if (Route::has('login'))
+                                <li class="mr-12">
+                                    <a class="nav-link" href="{{ route('login') }}">Zaloguj się</a>
                                 </li>
-                            @endguest
-                        </ul>
-                    </div>
+                            @endif
+
+                            @if (Route::has('register'))
+                                <li >
+                                    <a class="nav-link" href="{{ route('register') }}">Zarejestruj się</a>
+                                </li>
+                            @endif
+                        @else
+                            <div class="bg-white flex flex-col justify-center">
+                                <div class="flex items-center justify-center">
+                                    <div class="relative inline-block text-left dropdown">
+                                        <span class="rounded-md shadow-sm"
+                                        ><button class="inline-flex justify-center w-full px-4 py-2 text-sm font-medium leading-5 text-gray-700 transition duration-150 ease-in-out bg-white border border-gray-300 rounded-md hover:text-gray-500 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue active:bg-gray-50 active:text-gray-800"
+                                                 type="button" aria-haspopup="true" aria-expanded="true" aria-controls="headlessui-menu-items-117">
+                                            <span>Moje Konto</span>
+                                            <svg class="w-5 h-5 ml-2 -mr-1" viewBox="0 0 20 20" fill="currentColor"><path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+                                            </button
+                                            ></span>
+                                        <div class="opacity-0 invisible dropdown-menu transition-all duration-300 transform origin-top-right -translate-y-2 scale-95">
+                                            <div class="absolute right-0 w-56 mt-2 origin-top-right bg-white border border-gray-200 divide-y divide-gray-100 rounded-md shadow-lg outline-none" aria-labelledby="headlessui-menu-button-1" id="headlessui-menu-items-117" role="menu">
+                                                <div class="px-4 py-3">
+                                                    <p class="text-sm leading-5">Zalogowany jako</p>
+                                                    <p class="text-sm font-medium leading-5 text-gray-900 truncate">{{ Auth::user()->email }}</p>
+                                                </div>
+                                                <div class="py-1">
+                                                    <a href="/projects" tabindex="0" class="text-gray-700 flex justify-between w-full px-4 py-2 text-sm leading-5 text-left"  role="menuitem" >Projekty</a>
+                                                    <a href="#" tabindex="1" class="text-gray-700 flex justify-between w-full px-4 py-2 text-sm leading-5 text-left"  role="menuitem" >Ustawienia</a>
+                                                </div>
+                                                <div class="py-1">
+                                                    <a tabindex="3" href="{{ route('logout') }}"
+                                                       class="text-gray-700 flex justify-between w-full px-4 py-2 text-sm leading-5 text-left"  role="menuitem"
+                                                       onclick="event.preventDefault();
+                                                       document.getElementById('logout-form').submit();">
+                                                        {{ __('Wyloguj') }}
+                                                    </a>
+
+                                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                                        @csrf
+                                                    </form>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                        @endguest
+                    </ul>
                 </div>
             </div>
-        </nav>
+        </div>
+    </nav>
 
-        <main class="container mx-auto py-4">
-            @yield('content')
-        </main>
-    </div>
+    <main class="container mx-auto py-4">
+        @yield('content')
+    </main>
+</div>
 </body>
 </html>
